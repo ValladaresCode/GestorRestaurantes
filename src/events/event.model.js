22992ada05950522e29676d64c31024b17c7bba9
AngelM82 <amunoz-2025042@kinal.edu.gt>
2026-02-18 15:36:51 -0600

    C21 Finalización de modelo de DB

---
+++ src/events/event.model.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema(
+    {
+        reservationId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Reservation",
+            required: [true, "Reservation Id is required"]
+        },
+        description: {
+            type: String
+        },
+        photo: {
+            type: String,
+            default: null
+        },
+        startDate: {
+            type: Date,
+            required: [true, "Start date is required"]
+        },
+        endDate: {
+            type: Date,
+            required: [true, "End date is required"]
+        },
+
+        isActive: {
+            type: Boolean,
+            default: true
+        }
+
+    },
+
+    {
+        timestamps: true,
+        versionKey: false
+
+    }
+)
+
+eventSchema.index({ reservationId: 1 });
+eventSchema.index({ isActive: 1 });
+eventSchema.index({ reservationId: 1, isActive: 1 });
+export default mongoose.model("Event", eventSchema);
